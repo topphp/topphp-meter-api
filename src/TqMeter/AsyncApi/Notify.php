d40848f179bf455e8ed4479928b84928a61b7717
sleep <sleep@kaituocn.com>
2020-10-22 19:57:19 +0800

    增加订阅方法和单元测试

--- src/TqMeter/AsyncApi/Notify.php
+++ src/TqMeter/AsyncApi/Notify.php
@@ -14,6 +14,32 @@ use Topphp\TopphpMeterApi\TqMeter\Gateway;
 
 class Notify extends Gateway
 {
+    private $token = '';
+
+    /**
+     * @return string
+     */
+    public function getSubscribeToken(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     * @return Notify
+     */
+    public function setSubscribeToken(string $token): self
+    {
+        $this->token = $token;
+        return $this;
+    }
+
+    /**
+     * 异步通知回调
+     * @param $request
+     * @param Closure $callBack
+     * @author sleep
+     */
     public function send($request, Closure $callBack)
     {
         $sign            = $request["sign"];
@@ -27,5 +53,44 @@ class Notify extends Gateway
 //            echo 'SUCCESS';
             $callBack($responseContent);
         }
+    }
+
+    /**
+     * 订阅消息
+     * @param $response
+     * @param Closure $callBack
+     * @author sleep
+     */
+    public function subscribe($response, Closure $callBack)
+    {
+        $sign    = @getallheaders()["sign"];
+        $content = @file_get_contents('php://input');
+        if ($content == '') {
+            $callBack('返回数据为空', false);
+            echo 'SUCCESS';
+        } else {
+            if (!$this->checkSubscribeSign($content, $sign)) {
+//                echo "sign check failed";
+                $callBack('签名错误', false);
+            } else {
+//                echo 'SUCCESS';
+                $callBack($content, true);
+            }
+        }
+    }
+
+    /**
+     * 订阅消息签名验证方法
+     * @param $content
+     * @param $sign
+     * @return bool
+     * @author sleep
+     */
+    public function checkSubscribeSign($content, $sign)
+    {
+        // 随机字符串 后台获取
+        $buf    = $content . $this->getSubscribeToken();
+        $encode = md5($buf);
+        return $encode == $sign;
     }
 }
--- tests/MeterTest.php
+++ tests/MeterTest.php
@@ -18,7 +18,7 @@ class MeterTest extends HttpTestCase
 {
     private $code   = '';
     private $nonce  = '';
-    private $notify = 'http://n.kaituocn.com/';
+    private $notify = 'http://vtheatre.n.kaituocn.com/';
 
     public function testCollectorAdd()
     {
@@ -111,6 +111,44 @@ class MeterTest extends HttpTestCase
         var_dump(json_decode($res['response_content']));
     }
 
+//    public function testEleWrite()
+//    {
+//        $gateway = new EleMeterClient();
+//        $res     = $gateway->setAuthCode($this->code)
+//            ->setNonce($this->nonce)
+//            ->setNotifyUrl($this->notify)
+//            ->eleWrite([
+//                []
+//            ]);
+//        var_dump($res);
+//        var_dump(json_decode($res['response_content'], true));
+//    }
+
+    public function testEleSecurityOpenAccount()
+    {
+        $gateway = new EleMeterClient();
+        $res     = $gateway->setAuthCode($this->code)
+            ->setNonce($this->nonce)
+            ->setNotifyUrl($this->notify)
+            ->eleSecurityOpenAccount([
+                [
+                    'opr_id'      => $gateway->generateOperateId(),
+                    'cid'         => '200824015639',
+                    'address'     => '200824015639',
+                    'params'      => [
+                        'account_id' => '201931',
+                        'count'      => '1',
+                        'money'      => '100'
+                    ],
+                    'must_online' => true,
+                    'time_out'    => 60,
+                    'retry_times' => 1,
+                ]
+            ]);
+        var_dump($res);
+        var_dump(json_decode($res['response_content'], true));
+    }
+
     public function testCheckSign()
     {
 //        $a = '{"response_content":"[{\"opr_id\":\"a300fd302b16c07229fa00c9aad01cb2\",\"resolve_time\":\"2020-10-22 15:09:59\",\"status\":\"SUCCESS\",\"data\":[{\"type\":33,\"value\":[\"0.0000\"],\"dsp\":\"0.0000 kW\"}]}]","timestamp":"1603350601","sign":"0831f01586a764512a0e30dadcb5888c"}';
@@ -130,6 +168,19 @@ class MeterTest extends HttpTestCase
                 } else {
                     var_dump('签名错误');
                 }
+            });
+    }
+
+    public function testCheckSubscribe()
+    {
+        $_REQUEST['sign']    = '4297f44b13955235245b2497399d7a93';
+        $_REQUEST['content'] = '123';
+
+        $n = new Notify();
+        $n->setSubscribeToken('bn9twoNwg9k9ENc1lcxL')
+            ->subscribe($_REQUEST, function ($ret, $err) {
+                var_dump($ret);
+                var_dump($err);
             });
     }
 }
